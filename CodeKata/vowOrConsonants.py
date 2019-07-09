093afb582288999523c4e3de2f834f6c26a11054
Kichu <krishnaprasath9695@gmail.com>
2019-07-09 12:58:42 +0530

    Create vowOrConsonants.py

---
+++ CodeKata/vowOrConsonants.py
@@ -0,0 +1,12 @@
+#check whether a char is vowels or a cononant
+import re
+char =input()
+pattern = "[A-Za-z]"
+vowels =list("aeiou")
+if(re.match(pattern,char)):
+    if(char in vowels):
+        print("Vowels")
+    else:
+        print("Consonants")
+else:
+    print("Invalid")
