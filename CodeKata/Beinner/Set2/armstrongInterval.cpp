9de43944f711bc95c2e59089e94dfce57e3bde3d
Kichu <krishnaprasath9695@gmail.com>
2019-07-18 14:53:52 +0530

    Create armstrongInterval.cpp

---
+++ CodeKata/Beinner/Set2/armstrongInterval.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int checkArmStrong( int n);
+
+int main() {
+    int N,E;
+    cin>>N>>E;
+    while(N<E){
+        int ans=checkArmStrong(N+1);
+    if(ans==N+1){
+        cout<<ans;
+    }
+    N++;
+    }
+    
+    
+    return 0;
+}
+
+int checkArmStrong( int n){
+    int temp,result=0;
+    while(n>0){
+        temp=0;
+        temp=n%10;
+        
+        n/=10; 
+        result=result+pow(temp,3);
+        }
+    return result;
+}
