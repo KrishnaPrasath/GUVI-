5b89b6eeed9d65586d013ad8ba98fc832522729c
kishbi <krishnaprasath9695@gmail.com>
2019-07-25 11:27:07 +0530

    Update areBothOdd.js

--- CodeSpree/Exercise/areBothOdd.js
+++ CodeSpree/Exercise/areBothOdd.js
@@ -1,8 +1,8 @@
 function areBothOdd(num1, num2) {
   if ((num1 % 2 != 0) & (num2 % 2 != 0)) {
-    print("true");
+    console.log("true");
   } else {
-    print("false");
+    console.log("false");
   }
 }
 var ip1 = 15;
