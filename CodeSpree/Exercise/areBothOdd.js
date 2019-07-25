a207e8233af55da2203763160d8276a8ef824ee9
kishbi <krishnaprasath9695@gmail.com>
2019-07-25 11:24:47 +0530

    Create areBothOdd.js

---
+++ CodeSpree/Exercise/areBothOdd.js
@@ -0,0 +1,10 @@
+function areBothOdd(num1, num2) {
+  if ((num1 % 2 != 0) & (num2 % 2 != 0)) {
+    print("true");
+  } else {
+    print("false");
+  }
+}
+var ip1 = 15;
+var ip2 = 25;
+areBothOdd(ip1, ip2);
