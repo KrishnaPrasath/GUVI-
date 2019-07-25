3f2208f9e0ce38edd8520a2b2425148245d484ce
kishbi <krishnaprasath9695@gmail.com>
2019-07-25 11:42:19 +0530

    Create isSameLength.js

---
+++ CodeSpree/Exercise/isSameLength.js
@@ -0,0 +1,10 @@
+function isSameLength(word1, word2) {
+  if (word1.length == word2.length) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
+var name1 = "Revanth";
+var name2 = "shankar";
+isSameLength(name1, name2);
