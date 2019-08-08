5dcf8ee91b4ba91a6a7a3c51d8b6bbdc0dab38b9
kishbi <krishnaprasath9695@gmail.com>
2019-08-08 17:37:10 +0530

    Create findMedian.cpp
    I'm not sure about returning a single integer result of a median for a even-numbered list.

---
+++ CodeKata/Beinner/Set3/findMedian.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void findMedian(int sortedArray[], int N);
+int main()
+{
+    int N;
+
+    cin >> N;
+    int ip[N];
+    for (int i = 0; i < N; i++)
+    {
+        cin >> ip[N];
+    }
+    findMedian(ip, N);
+}
+
+void findMedian(int sortedArray[], int N)
+{
+
+    int size = N;
+    int median;
+    if ((size % 2) != 0)
+    {
+        median = sortedArray[(size / 2)];
+    }
+    else
+    {
+        median = (sortedArray[(size / 2) - 1] + sortedArray[size / 2]) / 2;
+    }
+    cout << median;
+}
