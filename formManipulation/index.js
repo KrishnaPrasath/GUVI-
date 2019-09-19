1917a52cb16bfd51dd5b206814cbf4a227f56069
Murdoc <krishnaprasath9695@gmail.com>
2019-09-19 20:18:08 +0530

    Add files via upload
    Form manipulation

---
+++ formManipulation/index.js
@@ -0,0 +1,8 @@
+// We are going to use the below Jquery UI library themeroller
+// 1.Selectable
+$(document).ready(function() {
+  $("#date").datepicker();
+  $("#selectable").selectable();
+  $("#sortable").sortable();
+  $("#sortable").disableSelection();
+});
